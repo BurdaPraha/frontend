ea6db3ce1e8bc679a58aa75846cb01db45d727fd
Michal Landsman <landsman@insuit.cz>
2017-07-10 14:19:06 +0200

    Added gulp for testing, little refactoring about naming;

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Frontend
 library with useful sass, javascript for projects @ BurdaPraha
+
+## Contributing
+before you'll send commit / PR please run: `npm install` and check if gulp end with success
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const fs            = require('fs');
+var gulp            = require('gulp'),
+    runSequence     = require('run-sequence'),
+    concat		    = require('gulp-concat'),
+    sass		    = require('gulp-sass'),
+    sourcemaps	    = require('gulp-sourcemaps'),
+    prefixer	    = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function (cb)
+{
+        return gulp
+            .src('./assets/styles/base.scss')
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(prefixer({
+                browsers: ['last 3 versions', 'IE 9'],
+                cascade: false
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(concat('style.css'))
+            .pipe(gulp.dest('./build/css'));
+});
+
+
+gulp.task('default', function()
+{
+    runSequence('sass');
+});

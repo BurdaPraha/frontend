bfb88f52b046dcc8ab13e27656a572b046e7ffd5
Michal Landsman <landsman@insuit.cz>
2017-08-22 17:54:44 +0200

    few new JS functions

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -49,7 +49,95 @@
             s.url        = url;
 
             document.body.appendChild(s);
+        },
+
+
+        /**
+         * Tabs <element onclick="switchContent(this, 'tab-xy');">
+         * @param e this - context
+         * @param elementId - target element id
+         */
+        switchContent: function (e, elementId)
+        {
+            var another_links = e.parentNode.children;
+            for(var l = 0; l < another_links.length; l++)
+            {
+                another_links[l].classList.remove('active');
+            }
+            e.classList.add('active');
+
+            // content
+            var target_content  = document.getElementById(elementId);
+            var another_content = target_content.parentNode.children;
+
+            for(var c = 0; c < another_content.length; c++)
+            {
+                another_content[c].classList.remove('active');
+                another_content[c].classList.add('hidden');
+            }
+
+            // set active tab
+            target_content.classList.remove('hidden');
+            target_content.classList.add('active');
+        },
+
+
+        /**
+         * @param element
+         * @returns {*}
+         */
+        getLabelText: function (element)
+        {
+            return element.getElementsByTagName("label")[0].innerHTML;
+        },
+
+
+        /**
+         *
+         * @param p
+         * @returns {boolean}
+         */
+        isValidSlovakPhone: function (p) {
+            var phoneRe = /^(\+?421)? ?[1-9][0-9]{2} ?[0-9]{3} ?[0-9]{3}$/;
+            var digits = p.replace(/\D/g, "");
+            return phoneRe.test(digits);
+        },
+
+
+        /**
+         * alternative searching element in same parent without jQuery
+         * @param el
+         * @param selector
+         * @returns {*}
+         */
+        closest: function (el, selector)
+        {
+            var matchesFn;
+
+            // find vendor prefix
+            ['matches','webkitMatchesSelector','mozMatchesSelector','msMatchesSelector','oMatchesSelector'].some(function(fn) {
+                if (typeof document.body[fn] == 'function') {
+                    matchesFn = fn;
+                    return true;
+                }
+                return false;
+            });
+
+            var parent;
+
+            // traverse parents
+            while (el) {
+                parent = el.parentElement;
+                if (parent && parent[matchesFn](selector)) {
+                    return parent;
+                }
+                el = parent;
+            }
+
+
+            return null;
         }
+
 
     };
 
